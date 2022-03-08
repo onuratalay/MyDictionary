ec9154b20f1a7cc6cb090c59fbd8ad4a4d281c06
ONUR ATALAY <onuratalay@ogr.trakya.edu.tr>
2022-03-09 00:01:35 +0300

    Innitial commit

---
+++ MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries
+{
+    internal class MyDictionary<T1, T2>
+    {
+        T1[] numbers;
+        T2[] names;
+        T1[] tempNumbers;
+        T2[] tempNames;
+
+        public MyDictionary()
+        {
+            numbers = new T1[0];
+            names = new T2[0];
+        }
+        public void Add(T1 number, T2 name)
+        {
+            tempNumbers = numbers;
+            numbers = new T1[numbers.Length + 1];
+            for (int i = 0; i < tempNumbers.Length; i++)
+            {
+                numbers[i] = tempNumbers[i];
+            }
+
+            tempNames = names;
+            names = new T2[names.Length + 1];
+            for (int i = 0; i < tempNames.Length; i++)
+            {
+                names[i] = tempNames[i];
+            }
+        }
+
+
+        public int Count
+        {
+            get { return numbers.Length; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,40 +21,4 @@ namespace Dictionaries
             Console.WriteLine(ogrenciler2.Count);
         }
     }
-
-    internal class MyDictionary<T1,T2>
-    {
-        T1[] numbers;
-        T2[] names;
-        T1[] tempNumbers;
-        T2[] tempNames;
-
-        public MyDictionary()
-        {
-            numbers = new T1[0];
-            names = new T2[0];
-        }
-        public void Add(T1 number, T2 name)
-        {
-            tempNumbers = numbers;
-            numbers = new T1[numbers.Length + 1];
-            for (int i = 0; i < tempNumbers.Length; i++)
-            {
-                numbers[i] = tempNumbers[i];
-            }
-
-            tempNames = names;
-            names = new T2[names.Length + 1];
-            for (int i = 0; i < tempNames.Length; i++)
-            {
-                names[i] = tempNames[i];
-            }
-        }
-
-
-        public int Count
-        {
-            get { return numbers.Length; }
-        }
-    }
 }
